cc9a4ab0bdd8a9e4a15dffcddaef55f620f23694
superelectriccharlie <superelectriccharlie@gmail.com>
2021-03-17 15:58:15 -0700

    Added slalom path

--- SRF-2021-codev2/src/main/java/frc/robot/Robot.java
+++ SRF-2021-codev2/src/main/java/frc/robot/Robot.java
@@ -222,10 +222,16 @@ public class Robot extends TimedRobot {
      
 
 
-     
-      driveBase.AutoDriveCalculation(90.0,0.05,1.0,1);
-
-     
+     //slalom
+      driveBase.AutoDriveCalculation(135.0,0.50,84.852814,1);
+      driveBase.AutoDriveCalculation(90.0,0.75,157.0,2);
+      driveBase.AutoDriveCalculation(33.18481568, 0.50, 41.672083,3);
+      driveBase.AutoDriveCalculation(90, 0.50, 37, 4);
+      driveBase.AutoDriveCalculation(180, 0.5, 34.75, 5);
+      driveBase.AutoDriveCalculation(270, 0.5, 37, 6);
+      driveBase.AutoDriveCalculation(326.8151843, 0.5, 41.672083, 7);
+      driveBase.AutoDriveCalculation(270, 0.75, 157, 8);
+      driveBase.AutoDriveCalculation(228.3527175, 0.5, 89.938868, 9);;
    }
    
    
